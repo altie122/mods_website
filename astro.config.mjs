a68719de3505f3d708b1fac42f8494117caf0b64
altie122 <altielynn@gmail.com>
2025-03-14 23:59:03 -0600

    add version Id for altie122.xyz/projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://mods.altie122.xyz',
+  site: 'https://minecraft.altie122.xyz',
   integrations: [react(), sitemap(), tailwind({
     applyBaseStyles: false
   })],
